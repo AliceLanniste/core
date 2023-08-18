fd518643f2022928d26b52f121c808baada2a579
彭思思 <1399789151@qq.com>
2023-08-19 00:48:00 +0800

    feat(compiler-sfc): support mapped types, string types & template types & built-in type

--- packages/compiler-sfc/__tests__/compileScript/resolveType.spec.ts
+++ packages/compiler-sfc/__tests__/compileScript/resolveType.spec.ts
@@ -190,9 +190,30 @@ describe('resolveType', () => {
     })
   })
 
-  // describe('built-in utility types', () => {
+  test('utility type: Pick', () => {
+    expect(
+      resolve(`
+    type T = { foo: number, bar: string, baz: boolean }
+    type K = 'foo' | 'bar'
+    type Target = Pick<T, K>
+    `).elements
+    ).toStrictEqual({
+      foo: ['Number'],
+      bar: ['String']
+    })
+  })
 
-  // })
+  test('utility type: Omit', () => {
+    expect(
+      resolve(`
+    type T = { foo: number, bar: string, baz: boolean }
+    type K = 'foo' | 'bar'
+    type Target = Omit<T, K>
+    `).elements
+    ).toStrictEqual({
+      baz: ['Boolean']
+    })
+  })
 
   describe('errors', () => {
     test('error on computed keys', () => {
